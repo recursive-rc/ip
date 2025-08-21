7635a675c8b241eed44d29ebd4372ecf66deeccf
recursive-rc <178076079+recursive-rc@users.noreply.github.com>
2025-08-21 21:43:07 +0800

    Improve skeletal version to add echo and exit command
    Chatbot now echos user's commands and exits when given command `bye`.

--- src/main/java/IttyBotty.java
+++ src/main/java/IttyBotty.java
@@ -1,15 +1,35 @@
+import java.util.Scanner;
+
 public class IttyBotty {
     private static final String CHATBOT_NAME = "Itty-Botty";
+    private static final String EXIT_COMMAND = "bye";
     
     public static void main(String[] args) {
         IttyBotty.greetUser();
-        IttyBotty.exit();
+        
+        boolean hasExited = false;
+        Scanner scanner = new Scanner(System.in);
+        while (!hasExited) {
+            String userInput = scanner.nextLine();
+            if (userInput.equals(IttyBotty.EXIT_COMMAND)) {
+                IttyBotty.exit();
+                hasExited = true;
+            } else {
+                IttyBotty.echo(userInput);
+            }
+        }
     }
     
     private static void greetUser() {
         IttyBotty.printHorizontalLine();
         System.out.println("Hello! I'm " + IttyBotty.CHATBOT_NAME);
         System.out.println("What can I do for you?");
+        IttyBotty.printHorizontalLine();
+    }
+    
+    private static void echo(String userInput) {
+        IttyBotty.printHorizontalLine();
+        System.out.println(userInput);
         IttyBotty.printHorizontalLine();
     }
     
