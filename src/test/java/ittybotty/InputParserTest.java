d0dca6e217acb4a271499267a55755bd6ab24ce7
recursive-rc <178076079+recursive-rc@users.noreply.github.com>
2025-09-03 11:11:08 +0800

    Add dummy JUnit test

---
+++ src/test/java/ittybotty/InputParserTest.java
@@ -0,0 +1,12 @@
+package ittybotty;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class InputParserTest {
+    @Test
+    public void dummyTest() {
+        assertEquals(2, 2);
+    }
+}
