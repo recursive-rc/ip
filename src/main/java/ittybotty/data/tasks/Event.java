ccba16954888bcbad897abf087ecc522d959359f
recursive-rc <178076079+recursive-rc@users.noreply.github.com>
2025-09-04 03:04:34 +0800

    Add JavaDoc comments to classes modelling tasks

--- src/main/java/ittybotty/data/tasks/Event.java
+++ src/main/java/ittybotty/data/tasks/Event.java
@@ -4,11 +4,20 @@ import ittybotty.OutputFormatter;
 
 import java.time.LocalDate;
 
+/**
+ * Represents an event with a start and end date.
+ */
 public class Event extends Task {
     private LocalDate startDate;
     private LocalDate endDate;
     // TODO: convert the above to proper date/time types
-    
+
+    /**
+     * Constructs an event.
+     * @param name Event description.
+     * @param startDate Event start date.
+     * @param endDate Event end date.
+     */
     public Event(String name, LocalDate startDate, LocalDate endDate) {
         super(name);
         this.startDate = startDate;
