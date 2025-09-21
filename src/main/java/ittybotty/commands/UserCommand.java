84ffbe5fb61a9f449bc44592c10f360f64b7dcf4
recursive-rc <178076079+recursive-rc@users.noreply.github.com>
2025-09-21 23:00:28 +0800

    Remove unused toString() implementation

--- src/main/java/ittybotty/commands/UserCommand.java
+++ src/main/java/ittybotty/commands/UserCommand.java
@@ -4,8 +4,5 @@ package ittybotty.commands;
  * Represents a user command.
  */
 public abstract class UserCommand {
-    @Override
-    public String toString() {
-        return "ittybotty.commands.UserCommand{}";
-    }
+
 }
